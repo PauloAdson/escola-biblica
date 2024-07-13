56beab5d8c5eac544e8e5a5753b0f9156866f969
Paulo Adson <paulo.alves200581@gmail.com>
2024-07-13 16:32:52 -0300

    iniciando o componente home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import './styles.css';
 
 export class Home extends React.Component {
     render() {
         return (
-            <></>
+            <>
+                <main>
+                    <section></section>
+                </main>
+            </>
         );
     }
 }
