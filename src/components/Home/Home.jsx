f792cd25ad4b3095741f900a7d16e180be7d7147
Paulo Adson <paulo.alves200581@gmail.com>
2024-07-13 20:36:07 -0300

    iniciando a seção visão geral do curso

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './styles.css';
-import { Botton } from "../Botton/Botton";
+import { Button } from "../Button/Button";
 
 export class Home extends React.Component {
     render() {
@@ -12,9 +12,18 @@ export class Home extends React.Component {
                             sobre a Bíblia
                         </h1>
                         <p className="uppercase">Através de lições Bíblicas</p>
-                        <Botton estilo="btn-destaque cadastro" link="./" texto="Começar Agora" />
+                        <Button estilo="btn-destaque cadastro" link="./" texto="Começar Agora" />
                     </section>
                 </main>
+                <section className="visao-geral-curso">
+                    <h2>Descubra a Profundidade das Escrituras</h2>
+                    <div className="visao-geral-curso__conteudo">
+                        <p>
+                            Nosso curso de estudo bíblico foi desenvolvido para todos aqueles que desejam aprofundar seu conhecimento e entendimento da Bíblia Sagrada. Seja você um iniciante ou alguém que já possui um conhecimento sólido das Escrituras, nosso curso  oferece um ambiente enriquecedor para explorar a Palavra de Deus de maneira profunda e significativa.
+                        </p>
+                        <img src="./algo" alt="imagem" />
+                    </div>
+                </section>
             </>
         );
     }
