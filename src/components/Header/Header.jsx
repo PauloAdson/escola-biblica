7b247839d81bfd386e8f2e49997930ee0b2f8ec9
Paulo Adson <paulo.alves200581@gmail.com>
2024-07-13 17:28:34 -0300

    arrumando a identacao

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,11 +10,11 @@ export class Header extends React.Component {
                 <nav className="menu">
                     <img src={logo} alt="Logo EBD" />
 
-                        <div className="menu-links">
-                            <Botton estilo="btn-header login" link="./" texto="Entrar" />
-                            <Botton estilo="btn-header cadastro" link="./" texto="Cadastrar-se" />
-                        </div>
-                       
+                    <div className="menu-links">
+                        <Botton estilo="btn-header login" link="./" texto="Entrar" />
+                        <Botton estilo="btn-header cadastro" link="./" texto="Cadastrar-se" />
+                    </div>
+
                 </nav>
             </header>
         );
