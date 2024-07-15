0ce212bcc833ad7078dbf14714839be926553116
Paulo Adson <paulo.alves200581@gmail.com>
2024-07-15 17:52:47 -0300

    refazendo o footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,29 +7,29 @@ export class Footer extends React.Component {
         return (
             <>
                 <footer>
-                    <div className="footer__content">
-                        <div className="footer__content-logo footer__content-height">
-                            <a href="/"><img src={logo} alt="Logo EBD" /></a>
-                            <p>Os céus e a terra passarão, mas as minhas palavras jamais passarão.</p>
+                    <div className="footer__container">
+
+                        <div className="footer__logo ">
+                            <img src={logo} alt="Logo EBD" />
+                            <p>
+                                Os céus e a terra passarão, mas as<br />
+                                minhas palavras jamais passarão.
+                            </p>
                         </div>
-
-                        <div className="footer__content-links footer__links-rapidos footer__content-height">
-                            <h4>Links Rápidos</h4>
-
-                            <p><a href="">Voltar ao topo</a></p>
-                            <p><a href="#duvidas">Dúvidas</a></p>
+                        <div className="footer__links">
+                            <h4 className="footer__links-titulo">Links Rápidos</h4>
+                            <a href="">Voltar ao topo</a>
+                            <a href="">Dúvidas</a>
                         </div>
-
-                        <div className="footer__content-links footer__content-height">
-                            <h4>Contatos</h4>
-
-                            <p><a href="https://wa.me/5562995132226" target="_blank">WhatsApp</a></p>
-                            <p><a href="mailto:paulo.alves200581@gmail.com">E-mail</a></p>
+                        <div className="footer__links">
+                            <h4 className="footer__links-titulo">Contatos</h4>
+                            <p>WhatsApp: <a href="https://wa.me/5562995132226">(62)99513-2226</a></p>
+                            <p>E-mail: <a href="mailto:paulo.alves200581@gmail.com">paulo.alves200581@gmail.com</a></p>
                         </div>
                     </div>
-
                 </footer>
-                <div className="footer__copyright"><p> Desenvolvido por <a href="https://portfolio-pauloadson.vercel.app/" target="_blank">Paulo Adson</a></p>
+                <div className="copyright">
+                    <p>Desenvolvido por <a href="https://portfolio-pauloadson.vercel.app/">Paulo Adson</a></p>
                 </div>
             </>
         );
