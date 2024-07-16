3a97dc933899db33aa739d64312d7e10b241384c
Paulo Adson <paulo.alves200581@gmail.com>
2024-07-15 23:48:22 -0300

    adicionando o link de cadastro aos botões de destaque

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -13,7 +13,7 @@ export class Home extends React.Component {
                             sobre a Bíblia
                         </h1>
                         <p className="uppercase">Através de lições Bíblicas</p>
-                        <Button estilo="btn-destaque cadastro" link="./" texto="Começar Agora" />
+                        <Button estilo="btn-destaque cadastro" link="/cadastro" texto="Começar Agora" />
                     </section>
                 </main>
 
@@ -25,7 +25,7 @@ export class Home extends React.Component {
                         </p>
                         <img className="visao-geral-curso__conteudo-img mobile-none" src="https://raw.githubusercontent.com/PauloAdson/escola-biblica/main/src/assets/images/imagem-bliblia.png" alt="imagem de uma bíblia" />
                     </div>
-                    <Button estilo="btn-destaque cadastro" link="./" texto="Começar Agora" />
+                    <Button estilo="btn-destaque cadastro" link="/cadastro" texto="Começar Agora" />
                 </section>
 
                 <section id="duvidas" className="duvidas">
